7ff7ac8feb8c6b69b463b46a0075be91bf5038aa
NaderSalameh <74309258+NaderSalameh@users.noreply.github.com>
2021-04-27 14:27:35 -0400

    Update logic.js

--- leaflet-challenge/static/js/logic.js
+++ leaflet-challenge/static/js/logic.js
@@ -64,7 +64,7 @@ d3.json("plates.json").then((json) =>
                     
                 }).bindPopup(`  <h2> ${quake.properties.place} </h2> <hr>
                                 <h3> Magnitude: ${quake.properties.mag} </h3>
-                                <h3> Depth: ${quake.geometry.coordinates[2]} km
+                                <h3> Depth: ${quake.geometry.coordinates[2]}km
                                 <h3> Time: ${dateConversion(quake.properties.time)} </h3>`)
             ) 
         }) 
@@ -148,7 +148,7 @@ d3.json("plates.json").then((json) =>
           legend.onAdd = function() {
             var div = L.DomUtil.create("div", "legend");
         
-            var depth = ['-10 km (BGS) ', '10 km (BGS)', `30 km (BGS)`, `50 km (BGS)`, `70 km (BGS)`, `90 km (BGS)`];
+            var depth = [`-10km (BGS) `, `10km (BGS)`, `30km (BGS)`, `50km (BGS)`, `70km (BGS)`, `90km (BGS)`];
             var depthColor = ["#5BFF33", "#FFC300", "#FF5733", "#C70039", "#900C3F", "#581845"];
         
         
