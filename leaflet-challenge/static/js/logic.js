8d104e0d06db3259f15513a4251cf8b4a0151005
NaderSalameh <74309258+NaderSalameh@users.noreply.github.com>
2021-04-27 15:14:45 -0400

    Update logic.js

--- leaflet-challenge/static/js/logic.js
+++ leaflet-challenge/static/js/logic.js
@@ -148,14 +148,14 @@ d3.json("plates.json").then((json) =>
           legend.onAdd = function() {
             var div = L.DomUtil.create("div", "legend");
         
-            var depth = [`-10km (BGS)`, `10km (BGS)`, `30km (BGS)`, `50km (BGS)`, `70km (BGS)`, `90km (BGS)`];
+            var depth = [`-10km`, `10km`, `30km`, `50km`, `70km`, `90km`];
             var depthColor = ["#5BFF33", "#FFC300", "#FF5733", "#C70039", "#900C3F", "#581845"];
         
         
             for (var i = 0; i < depth.length; i++) {
               div.innerHTML +=
               "<i style='background: " + depthColor[i] + "'></i> " +
-              depth[i] + (depth[i + 1] ? "&ndash;" + depth[i + 1] + "<br>" : "+");
+              depth[i] + (depth[i + 1] ? " &ndash; " + depth[i + 1] + "<br>" : "+");
             }
             return div;
         
